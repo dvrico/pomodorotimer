b2971679d726322e86eca7535c57eee054abbf9b
dvrico <rico774@gmail.com>
2015-08-21 17:20:24 -0400

    Add timer functionality. Switch out this and setInterval for  and

--- app.js
+++ app.js
@@ -1,24 +1,53 @@
 (function() {
   var myApp = angular.module('myApp', []);
 
-  myApp.controller('MainController', function(){
-    this.breakLength = 5
-    this.sessionLength = 25
+  myApp.controller('MainController', ['$scope', '$interval', function($scope, $interval){
+    $scope.breakLength = 5
+    $scope.sessionLength = 25
+    $scope.timeLeft = $scope.sessionLength
+    $scope.sessionName = 'Session'
 
-    this.breakLengthChange = function(time) {
-      this.breakLength += time
-      if (this.breakLength < 0) {
-        this.breakLength = 0
+    var timerIsRunning = false
+    var secs = 60 * $scope.timeLeft
+
+    $scope.breakLengthChange = function(time) {
+      $scope.breakLength += time
+      if ($scope.breakLength < 0) {
+        $scope.breakLength = 0
       }
     }
 
-    this.sessionLengthChange = function(time) {
-      this.sessionLength += time
-      if (this.sessionLength < 0) {
-        this.sessionLength = 0
+    $scope.sessionLengthChange = function(time) {
+      if(!timerIsRunning) {
+        $scope.sessionLength += time
+        if ($scope.sessionLength < 0) {
+          $scope.sessionLength = 0
+        }
+        $scope.timeLeft = $scope.sessionLength
       }
     }
 
-  }) // End of MainController
+    $scope.toggleTimer = function() {
+      console.log(1)
+      if (!timerIsRunning) {
+        if ($scope.currentName === 'Session') {
+          $scope.currentLength = $scope.sessionLength
+        } else {
+          $scope.currentLength = $scope.breakLength
+        }
+
+        updateTimer()
+        timerIsRunning = $interval(updateTimer, 1000)
+      }
+    }
+
+    function updateTimer() {
+      secs -= 1
+      console.log(secs)
+      $scope.timeLeft = secs
+      console.log($scope.timeLeft)
+    }
+
+  }]) // End of MainController
 
 }()); // End of IIFE
